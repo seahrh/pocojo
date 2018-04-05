9859c0f981e26d1327ca4180f82c6d9e9f96b92e
Ruhong <seahrh@gmail.com>
2018-04-05 22:37:04 +0800

    add tests strip_html, fix import error

--- etl/tests/test_markup_remover.py
+++ etl/tests/test_markup_remover.py
@@ -6,9 +6,12 @@ class TestMarkupRemover(unittest.TestCase):
 
     def test_strip_html(self):
         self.assertEqual(strip_html(''), '')
+        self.assertEqual(strip_html('ab'), 'ab')
         self.assertEqual(strip_html('<p>p</p>'), 'p')
-        # self.assertEqual(strip_html('a &nbsp; b'), 'a  b')
-        # self.assertEqual(strip_html('a&nbsp;b'), 'a&nbsp;b')
+        self.assertEqual(strip_html('<p class="blue">p</p>'), 'p')
+        self.assertEqual(strip_html('a&;b'), 'a&;b')
+        self.assertEqual(strip_html('a &; b'), 'a &; b')
+        self.assertEqual(strip_html('a &amp; b'), 'a & b')
 
 
 if __name__ == '__main__':
--- sklearnpd/sklearnpd.py
+++ sklearnpd/sklearnpd.py
@@ -1,4 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+import numpy as np
 
 
 class TextExtractor(BaseEstimator, TransformerMixin):
