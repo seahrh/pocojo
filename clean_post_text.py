641fa77ff7e13d365cc0a1cf32c50bbf4adaff6d
Ruhong <seahrh@gmail.com>
2018-04-28 08:30:57 +0800

    export pd dataframe to tsv, refactor extract method

--- clean_post_text.py
+++ clean_post_text.py
@@ -2,36 +2,75 @@ import glob
 import json
 from os import path
 
+import pandas as pd
 from etl.markup_remover import strip_html
 from stringx.stringx import to_ascii_str
 
 posts_glob_pattern = 'posts/*.json'
 file_path_template = 'posts_txt/p{}.txt'
 
-n = 0
-paths = glob.glob(posts_glob_pattern)
-for p in paths:
-    with open(p, 'rt') as f:
-        jo = json.load(f)
-        posts = jo['posts']
-        if not posts:
-            raise AssertionError('posts must not be empty')
-        post = posts[0]
-        title = post['title']
-        if not title:
-            raise AssertionError('title must not be empty')
-        content = post['content']
-        if not content:
-            raise AssertionError('content must not be empty')
-        author = post['author']['display_name'].replace(' ', '').lower()
-        if not content:
-            raise AssertionError('author must not be empty')
-        s = '{} {} {}'.format(author, title, content)
-        s = to_ascii_str(strip_html(s))
-        # print(f'{repr(s)}\n')
-        pid = path.basename(p)[1:-5]
-        file_path = file_path_template.format(pid)
-        with open(file_path, 'wt', encoding='utf8') as out:
-            out.write(s)
-        n += 1
-        print(f'n={n}, pid={pid}')
+
+def __title(jo):
+    posts = jo['posts']
+    if not posts:
+        raise AssertionError('posts must not be empty')
+    post = posts[0]
+    res = to_ascii_str(post['title'])
+    if not res:
+        raise AssertionError('title must not be empty')
+    return res
+
+
+def __content(jo):
+    posts = jo['posts']
+    if not posts:
+        raise AssertionError('posts must not be empty')
+    post = posts[0]
+    res = to_ascii_str(strip_html(post['content']))
+    if not res:
+        raise AssertionError('content must not be empty')
+    return res
+
+
+def __author(jo):
+    posts = jo['posts']
+    if not posts:
+        raise AssertionError('posts must not be empty')
+    post = posts[0]
+    res = to_ascii_str(post['author']['display_name']).replace(' ', '').lower()
+    if not res:
+        raise AssertionError('author must not be empty')
+    return res
+
+
+def __save_text_file(file_path, text):
+    with open(file_path, 'wt', encoding='utf8') as out:
+        out.write(text)
+
+
+def __main():
+    df = pd.DataFrame(columns=['pid', 'author', 'text'])
+    n = 0
+    paths = glob.glob(posts_glob_pattern)
+    for p in paths:
+        with open(p, 'rt') as f:
+            pid = path.basename(p)[1:-5]
+            n += 1
+            print(f'n={n}, pid={pid}')
+            jo = json.load(f)
+            title = __title(jo)
+            content = __content(jo)
+            author = __author(jo)
+            text = '{} {}'.format(title, content)
+            df = df.append({
+                'pid': pid,
+                'author': author,
+                'text': text
+            }, ignore_index=True)
+            # file_path = file_path_template.format(pid)
+            # __save_text_file(file_path, text)
+    df.to_csv("data.tsv", sep='\t')
+
+
+if __name__ == '__main__':
+    __main()
