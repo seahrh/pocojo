13dba44fb1b36e52d6109bade78b5f2ba70b089b
Ruhong <seahrh@gmail.com>
2018-04-30 08:14:20 +0800

    add comment_count column to dataframe

--- clean_post_text.py
+++ clean_post_text.py
@@ -6,8 +6,10 @@ import pandas as pd
 from etl.markup_remover import strip_html
 from stringx.stringx import to_ascii_str
 
-posts_glob_pattern = 'posts/*.json'
-file_path_template = 'posts_txt/p{}.txt'
+__posts_glob_pattern = 'posts/*.json'
+__file_path_template = 'posts_txt/p{}.txt'
+__comment_path_template = 'comments/c{}.json'
+__out_file_path = 'tmp/data.tsv'
 
 
 def __title(jo):
@@ -56,29 +58,44 @@ def __pids(paths):
     return res
 
 
+def __comment_count(file_path):
+    with open(file_path, 'rt') as f:
+        jo = json.load(f)
+        comments = jo['comments']
+        n = 0
+        for i, comment in enumerate(comments):
+            n += 1
+            for _ in comment['children']:
+                n += 1
+        return n
+
+
 def __main():
-    paths = glob.glob(posts_glob_pattern)
+    paths = sorted(glob.glob(__posts_glob_pattern))
     pids = __pids(paths)
-    df = pd.DataFrame(columns=['author', 'text'], index=pids)
+    df = pd.DataFrame(columns=['comment_count', 'author', 'text'], index=pids)
     n = 0
     for p in paths:
         with open(p, 'rt') as f:
             pid = path.basename(p)[1:-5]
             n += 1
-            print(f'n={n}, pid={pid}')
+            comment_path = __comment_path_template.format(pid)
+            comment_count = __comment_count(comment_path)
+            print(f'n={n}, pid={pid}, comment_count={comment_count}')
             jo = json.load(f)
             title = __title(jo)
             content = __content(jo)
             author = __author(jo)
             text = '{} {}'.format(title, content)
             df.loc[pid] = pd.Series({
+                'comment_count': comment_count,
                 'author': author,
                 'text': text
             })
             # file_path = file_path_template.format(pid)
             # __save_text_file(file_path, text)
     df = pd.get_dummies(df, columns=['author'], prefix=['author'])
-    df.to_csv("tmp/data.tsv", sep='\t')
+    df.to_csv(__out_file_path, sep='\t')
 
 
 if __name__ == '__main__':
