a6bad3075075d0f932789354d4c23e98f7eecb0e
Ruhong <seahrh@gmail.com>
2018-06-16 16:25:37 +0800

    packaging with setuptools

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+__version__ = '3.0'
+setup(
+    name="tia_pocojo",
+    version=__version__,
+    python_requires='>=3.6.0',
+    install_requires=[
+        'nltk>=3.2.5,<4',
+        'numpy>=1.14.2,<2',
+        'pandas>=0.22.0,<1',
+        'scikit-learn>=0.19.1,<1',
+        'scipy>=1.0.1,<2'
+    ],
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
+    description='description',
+    license='MIT'
+)
